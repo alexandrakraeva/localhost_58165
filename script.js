ae50104265b36cae59097837c950240f1b3fd145
alexandrakraeva <alexandra.work.kraeva@gmail.com>
2023-12-27 12:46:33 +0100

    hhh bvvdd

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+const socket = new WebSocket('ws://localhost:3000'); // WebSocket server address
+
+document.getElementById('yesBtn').addEventListener('click', () => {
+    socket.send('YES');
+});
+
+document.getElementById('noBtn').addEventListener('click', () => {
+    socket.send('NO');
+});
